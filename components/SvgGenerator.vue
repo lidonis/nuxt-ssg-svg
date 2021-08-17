4900c74f8f08412283205e29b353d0619881ca93
lidonis <lidonis.calhau@gmail.com>
2021-08-17 22:52:30 +0200

    component that generated svg on the client

--- components/SvgGenerator.vue
+++ components/SvgGenerator.vue
@@ -1,13 +1,22 @@
 <template>
-$END$
+  <svg id="generated-svg">
+    <title>{{title}}</title>
+  </svg>
 </template>
 
+
 <script>
+import rough from 'roughjs/bundled/rough.cjs.js';
+
 export default {
-name: "SvgGenerator"
+  props: ['title', 'nb', 'color'],
+  mounted() {
+    let svg = document.getElementById('generated-svg')
+    const rc = rough.svg(svg);
+    for (let i = 0; i < this.nb; i++) {
+      let node = rc.rectangle(100 * (i), 10, 80, 80, { fill: this.color });
+      svg.appendChild(node);
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
