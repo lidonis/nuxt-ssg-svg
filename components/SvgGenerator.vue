25ef6e64676414998f2581dba310d5b60de91263
lidonis <lidonis.calhau@gmail.com>
2021-08-17 22:49:11 +0200

    Initial commit

---
+++ components/SvgGenerator.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "SvgGenerator"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <SvgGenerator title="SVG from Index" nb="3" color="blue"></SvgGenerator>
+</template>
